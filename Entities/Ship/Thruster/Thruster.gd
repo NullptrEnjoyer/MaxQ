8c10c7a017c6770b7012c1572d2cec1b380522e4
DomagojJukic1RPE1 <134812871+NullptrEnjoyer@users.noreply.github.com>
2023-07-15 15:34:41 +0200

    Changed GDScript version for comparation benchmarking

--- Entities/Ship/Thruster/Thruster.gd
+++ Entities/Ship/Thruster/Thruster.gd
@@ -1,5 +1,7 @@
 class_name ThrusterTemp
 
+extends Node2D
+
 @export_group("Thruster Properties")
 @export var power: int = 100
 ##0 to 2
@@ -17,7 +19,7 @@ class_name ThrusterTemp
 #Turn off if I ever make vectored engines
 @export var precompute: bool = 1
 
-@onready var sprite: AnimatedSprite2D = $Layer0
+@onready var sprite: Sprite2D = $Sprite2D
 
 var main_body: RigidBody2D
 
@@ -71,45 +73,7 @@ func add_raycast_exception(node: CollisionObject2D) -> void:
 			child_node.add_exception(node)
 
 func handle_raycast_check(delta) -> void:
-	var scale_sprite_x: float = 1
-	
-	var point: Vector2
-	var dist: float
-	var mult: float
-	var what: CollisionObject2D
-	var ray_force: Vector2
-	
-	for node in ray_arr:
-		if ((node.is_colliding())):
-			point = node.get_collision_point()
-			dist = node.global_position.distance_to(point)
-			mult = 1 - dist / node.max_dist
-			what = node.get_collider()
-			
-			scale_sprite_x += mult / num_of_rays# / 3
-			
-			ray_force = precomp_force.rotated(main_body.global_rotation)
-			
-			#apply force to the main body
-			ray_force.x /= num_of_rays
-			ray_force.x *= mult * delta
-			ray_force.y /= num_of_rays
-			ray_force.y *= mult * delta
-			
-			offset.x = point.x - global_position.x
-			offset.y = point.y - global_position.y
-			
-			main_body.apply_force(ray_force, offset)
-			
-			ray_force.x *= -1
-			ray_force.y *= -1
-			
-			offset.x = point.x - what.global_position.x
-			offset.y = point.y - what.global_position.y
-			
-			what.apply_force(ray_force, offset)
-		
-	sprite.scale.x = scale_sprite_x
+	pass
 
 func get_torque() -> float:
 	return precomp_torque
@@ -127,15 +91,6 @@ func _ready():
 	power *= 20000000
 	
 	sprite.modulate = color
-	
-	if (number_of_sprites > 0):
-		var bro1: AnimatedSprite2D = sprite.duplicate()
-		setup_sprite_child(sprite, bro1, -10)
-		bro1.set_frame_and_progress(1, 0)
-		if (number_of_sprites > 1):
-			var bro2: AnimatedSprite2D = bro1.duplicate()
-			setup_sprite_child(bro1, bro2, -8)
-			bro2.set_frame_and_progress(2, 0)
 	
 	sprite.visible = 0
 	
